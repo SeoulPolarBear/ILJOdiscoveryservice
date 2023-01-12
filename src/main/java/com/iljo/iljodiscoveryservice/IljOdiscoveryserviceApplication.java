0eb6c223a4530b59c09c8799b55b658dffeb6e30
Whiteheim <gns240@gmail.com>
2023-01-12 14:52:33 +0900

    Eureka_heim

--- src/main/java/com/iljo/iljodiscoveryservice/IljOdiscoveryserviceApplication.java
+++ src/main/java/com/iljo/iljodiscoveryservice/IljOdiscoveryserviceApplication.java
@@ -3,6 +3,9 @@ package com.iljo.iljodiscoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +15,13 @@ public class IljOdiscoveryserviceApplication {
 		SpringApplication.run(IljOdiscoveryserviceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
